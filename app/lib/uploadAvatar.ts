9ca7f7a77223f1ef768de43463368d8c5543bcfb
Unam Bangani <unam.bangani@gmail.com>
2024-07-18 20:19:12 +0200

    added canvas

--- app/lib/uploadAvatar.ts
+++ app/lib/uploadAvatar.ts
@@ -1,51 +1,48 @@
 "use server";
 
-import { writeFile, unlink } from "fs/promises";
-import { readdir } from "fs/promises";
+import { writeFile } from "fs/promises";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import path from "path";
+import { createCanvas, loadImage } from "canvas";
 
 export async function UploadAvatar(formData: FormData) {
   console.log(formData.get("image"));
 
   const image: File = formData.get("image") as File;
   const userid = formData.get("userid");
-  const extension = image.name.split(".").pop()!.toLowerCase();
+  const extension = image.name.split(".").pop();
   const buffer = Buffer.from(await image.arrayBuffer());
 
-  if (image.size > 4e6 || image.size == 0) {
+  if (image.size > 4e6 || image.size === 0) {
     return;
   }
 
-  const uploadPath = process.env.PATH_TO_FILE!;
-  const newFilePath = path.join(uploadPath, `${userid}.${extension}`);
+  let newBuffer;
+  if (extension === "jpg" || extension === "jpeg") {
+    try {
+      const img = await loadImage(buffer);
+      const canvas = createCanvas(img.width, img.height);
+      const ctx = canvas.getContext("2d");
+
+      ctx.drawImage(img, 0, 0);
+      newBuffer = canvas.toBuffer("image/png");
+    } catch (error) {
+      console.log("Error during image conversion:", error);
+      return;
+    }
+  } else {
+    newBuffer = buffer;
+  }
+
+  const path = process.env.PATH_TO_FILE! + userid?.toString() + ".png";
 
   try {
-    // Delete any other image files with the same name but different extensions
-    const files = await readdir(uploadPath);
-    const baseFileName = userid?.toString();
-
-    for (const file of files) {
-      const fileExtension = path.extname(file).toLowerCase();
-      const fileNameWithoutExtension = path.basename(file, fileExtension);
-
-      if (
-        fileNameWithoutExtension === baseFileName &&
-        fileExtension !== `.${extension}` &&
-        (fileExtension === ".jpeg" ||
-          fileExtension === ".jpg" ||
-          fileExtension === ".png")
-      ) {
-        await unlink(path.join(uploadPath, file));
-      }
-    }
-
-    await writeFile(newFilePath, buffer);
-    console.log("File uploaded successfully");
+    await writeFile(path, newBuffer);
+    console.log("I win?");
   } catch (error) {
     console.log(error);
     return;
   }
-  revalidatePath("/settings", "layout");
+  revalidatePath("/settings, layout");
+  redirect("/settings");
 }
