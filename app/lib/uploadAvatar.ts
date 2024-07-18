33e31395f99e05a34ca154abe6dbe97c455e0535
Unam Bangani <unam.bangani@gmail.com>
2024-07-18 20:00:09 +0200

    removed sharp and no longer converts

--- app/lib/uploadAvatar.ts
+++ app/lib/uploadAvatar.ts
@@ -1,38 +1,48 @@
 "use server";
 
-import { writeFile } from "fs/promises";
+import { writeFile, unlink } from "fs/promises";
+import { readdir } from "fs/promises";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import sharp from "sharp";
+import path from "path";
 
 export async function UploadAvatar(formData: FormData) {
   console.log(formData.get("image"));
 
   const image: File = formData.get("image") as File;
   const userid = formData.get("userid");
-  const extension = image.name.split(".").pop();
+  const extension = image.name.split(".").pop()!.toLowerCase();
   const buffer = Buffer.from(await image.arrayBuffer());
 
   if (image.size > 4e6 || image.size == 0) {
     return;
   }
 
-  let path = process.env.PATH_TO_FILE! + userid?.toString();
+  const uploadPath = process.env.PATH_TO_FILE!;
+  const newFilePath = path.join(uploadPath, `${userid}.${extension}`);
 
   try {
-    let processedBuffer = buffer;
-    if (
-      extension!.toLowerCase() === "jpg" ||
-      extension!.toLowerCase() === "jpeg"
-    ) {
-      path += ".png";
-      processedBuffer = await sharp(buffer).png().toBuffer();
-    } else {
-      path += "." + extension;
+    // Delete any other image files with the same name but different extensions
+    const files = await readdir(uploadPath);
+    const baseFileName = userid?.toString();
+
+    for (const file of files) {
+      const fileExtension = path.extname(file).toLowerCase();
+      const fileNameWithoutExtension = path.basename(file, fileExtension);
+
+      if (
+        fileNameWithoutExtension === baseFileName &&
+        fileExtension !== `.${extension}` &&
+        (fileExtension === ".jpeg" ||
+          fileExtension === ".jpg" ||
+          fileExtension === ".png")
+      ) {
+        await unlink(path.join(uploadPath, file));
+      }
     }
 
-    await writeFile(path, processedBuffer);
-    console.log("I win?");
+    await writeFile(newFilePath, buffer);
+    console.log("File uploaded successfully");
   } catch (error) {
     console.log(error);
     return;
